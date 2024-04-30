3b24ea12ce419533c90e952b9df38e98e99898a3
Seth Ketsi <christambassador12@gmail.com>
2024-04-30 17:56:47 +0000

    0. Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" Cache module """
+
+import uuid
+import redis
+from typing import Union
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        """ Constructor """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
